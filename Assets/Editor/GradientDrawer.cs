8ff33f0411c77d2cda0aa53e903213c25e6182ab
Dalton Lima <dl@seemore.lu>
2019-06-20 19:28:53 +0200

    add Gradient Drawer

---
+++ Assets/Editor/GradientDrawer.cs
@@ -0,0 +1,52 @@
+using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(CustomGradient))]
+public class GradientDrawer : PropertyDrawer
+{
+    private const float MarginAfterLabel = 5f;
+
+    /*
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        //return base.GetPropertyHeight(property, label);
+        return customHeightInPixels;
+    }
+    */
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        //base.OnGUI(position, property, label);
+
+        Event guiEvent = Event.current;
+        CustomGradient gradient = (CustomGradient)fieldInfo.GetValue(property.serializedObject.targetObject);
+        float labelWidth = GUI.skin.label.CalcSize(label).x + MarginAfterLabel;
+        Rect textureRect = new Rect(position.x + labelWidth, position.y, position.width - labelWidth, position.height);
+
+        if (guiEvent.type == EventType.Repaint)
+          {
+            GUI.DrawTexture(textureRect, gradient.GetTexture((int)position.width));
+            GUI.Label(position, label.text);
+
+            // Workaround for background glitches (when moving cursor)
+            // Doesn't appear to happen on 2019.3.0a4
+#if !UNITY_2019_3_OR_NEWER
+            GUIStyle gradientStyle = new GUIStyle();
+            gradientStyle.normal.background = gradient.GetTexture((int) position.width);
+            GUI.Label(textureRect, GUIContent.none, gradientStyle);
+#endif
+        }
+        else
+        {
+            if (guiEvent.type == EventType.MouseDown && guiEvent.button == 0)
+            {
+                if (textureRect.Contains(guiEvent.mousePosition))
+                {
+                    // open editor window
+                    Debug.Log("Clicked on gradient");
+                }
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/CustomGradient.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class CustomGradient
+{
+    public Color Evaluate(float time)
+    {
+        return Color.Lerp(Color.white, Color.black, time);
+    }
+
+    public Texture2D GetTexture(int width)
+    {
+        Texture2D texture = new Texture2D(width, 1);
+        Color[] colors = new Color[width];
+        for (int i = 0; i < width; i++)
+        {
+            colors[i] = Evaluate((float)i / (width - 1));
+        }
+        texture.SetPixels(colors);
+        texture.Apply();
+        return texture;
+    }
+}
