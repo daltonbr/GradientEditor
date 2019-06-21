f5c86a438d6f4c48d2ff844656c34ff5b70ca9bc
Dalton Lima <dl@seemore.lu>
2019-06-21 17:44:36 +0200

    add ColorKeys to the gradient

--- Assets/Scripts/CustomGradient.cs
+++ Assets/Scripts/CustomGradient.cs
@@ -5,9 +5,70 @@ using UnityEngine;
 [System.Serializable]
 public class CustomGradient
 {
+    [SerializeField] private List<ColorKey> _keys = new List<ColorKey>();
+
+    ///<summary>Represents a key in the CustomGradient.</summary>
+    [System.Serializable]
+    public struct ColorKey
+    {
+        [SerializeField] private Color _color;
+        ///<summary>Normalized value position representing the position of the Key in the gradient.</summary>
+        [SerializeField] private float _time; 
+
+        public Color Color => _color;
+        public float Time => _time;
+
+        public ColorKey(Color color, float time)
+        {
+            _color = color;
+            _time = time;
+        }
+    }
+
+    public void AddKey(Color color, float time)
+    {
+        ColorKey newColorKey = new ColorKey(color, time);
+        for (int i = 0; i < _keys.Count ; i++)
+        {
+            if (newColorKey.Time < _keys[i].Time)
+            {
+                _keys.Insert(i, newColorKey);
+                return;
+            }
+        }
+        _keys.Add(newColorKey);
+    }
+
+    ///<summary>The amount of keys in the CustomGradient</summary>
+    public int NumKeys => _keys.Count;
+
+    public ColorKey GetKey(int index)
+    {
+        return _keys[index];
+    }
+
     public Color Evaluate(float time)
     {
-        return Color.Lerp(Color.white, Color.black, time);
+        if (_keys.Count == 0)
+        {
+            return Color.white;
+        }
+
+        ColorKey keyLeft = _keys[0];
+        ColorKey keyRight = _keys[_keys.Count - 1];
+
+        for (int i = 0; i < _keys.Count - 1; i++)
+        {
+            if (_keys[i].Time <= time && _keys[i+1].Time >= time)
+            {
+                keyLeft = _keys[i];
+                keyRight = _keys[i+1];
+                break;
+            }
+        }
+
+        float blendTime = Mathf.InverseLerp(keyLeft.Time, keyRight.Time, time);
+        return Color.Lerp(keyLeft.Color, keyRight.Color, blendTime);
     }
 
     public Texture2D GetTexture(int width)
--- Assets/Scripts/GradientEditor.cs
+++ Assets/Scripts/GradientEditor.cs
@@ -3,17 +3,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class GradientEditor : EditorWindow
 {
     private CustomGradient _gradient;
     private const int BorderSize = 10;
     private const int gradientPreviewRectHeight = 20;
+    private float _keyWidth = 10; 
+    private float _keyHeight = 20;
 
     private void OnGUI()
     {
+        Event guiEvent = Event.current;
+
         Rect gradientPreviewRect = new Rect(BorderSize, BorderSize, position.width - (BorderSize * 2), gradientPreviewRectHeight);
         GUI.DrawTexture(gradientPreviewRect, _gradient.GetTexture((int)gradientPreviewRect.width));
+
+        for (int i = 0; i < _gradient.NumKeys; i++)
+        {
+            CustomGradient.ColorKey key = _gradient.GetKey(i);
+            Rect keyRect = new Rect(gradientPreviewRect.x + (gradientPreviewRect.width * key.Time) - (_keyWidth/2f),
+            gradientPreviewRect.yMax + BorderSize, _keyWidth, _keyHeight);
+            EditorGUI.DrawRect(keyRect, key.Color);
+            Repaint();    
+        }
+
+        if (guiEvent.type == EventType.MouseDown && guiEvent.button == 0)
+        {
+            Color randomColor = new Color(Random.value, Random.value, Random.value);
+            var keyTime = Mathf.InverseLerp(gradientPreviewRect.x, gradientPreviewRect.xMax, guiEvent.mousePosition.x);
+            _gradient.AddKey(randomColor, keyTime); 
+        }
     }
 
     public void SetGradient(CustomGradient gradient)
